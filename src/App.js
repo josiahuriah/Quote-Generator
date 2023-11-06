88a94bcf4ee27be7d9569d8924eea82f1be67682
Josh <josh.duncanson@gmail.com>
2023-11-06 11:45:25 -0500

    Removed unnecessary lines and added styling

--- src/App.js
+++ src/App.js
@@ -1,25 +1,39 @@
-import logo from './logo.svg';
+import React, {useEffect, useState} from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+export default function App() {
+  const [quotes, setQuotes] = useState([]);
+  const limit = 15;
+
+  useEffect(() => {
+    if (quotes.length >= limit){
+      return;
+    }
+    fetch("https://type.fit/api/quotes")
+      .then((response) => response.json())
+      .then((data) => {
+        console.log(data)
+        setQuotes(prevPosts => [...prevPosts, ...data]);
+      })
+      .catch((err) => {
+        console.log(err.message);
+      }, [quotes, limit])
+  }, [quotes])
+
+  const quote = quotes[Math.floor(Math.random()*limit)];
+
+  const quoteText = quote ? quote.text : '';
+  const quoteAuthor = quote ? quote.author.slice(0, -10) : '';
+   
+  return(
+    <div className='container'>
+      <h1 className='title'>Quote Generator</h1>
+      <p className='quoteText'>{quoteText}</p>
+      <p className='quoteAuthor'>{quoteAuthor}</p>
+      <button
+      className='button' 
+      onClick={() => setQuotes([])}>Get New Quote</button>
     </div>
-  );
+  )
 }
 
-export default App;
